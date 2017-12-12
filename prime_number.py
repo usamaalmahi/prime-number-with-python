f1bc1ea34a54679871e5620ba32611901f818181
usamaalmahi <usamaalmahi@gmail.com>
2017-12-12 22:34:41 +0600

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,12 +1,12 @@
 num = input("Enter a number\n")
-mid=num/2
-flag=0
+mid = num/2
+flag = 0
 for i in range(2,mid):
     if num%i == 0:
-        flag=1
+        flag = 1
         break
-    i=i+1
-if flag==0:
-    print("this is prime number")
+    i = i+1
+if flag == 0:
+    print("this is a prime number")
 else:
-    print("this is not prime")
+    print("this is not a prime number")
